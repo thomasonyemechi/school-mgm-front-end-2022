65fd2339589188469f666c2e8c8ef7b814ca5227
Thomas Onyemechi <thomasonyemechi03@gmail.com>
2022-02-09 08:55:27 +0100

    result

--- resources/views/control/result_setup.blade.php
+++ resources/views/control/result_setup.blade.php
@@ -62,6 +62,85 @@
 
             </div>
 
+
+            <div class="col-md-4">
+                <div class="card card-secondary card-outline">
+                    <div class="card-header">
+                        <h3 class="card-title ">
+                            <i class="fa fa-book" aria-hidden="true"></i>
+                            Grades Setup
+                        </h3>
+                    </div>
+                    <div class="card-body p-1   ">
+                        <div class="table-responsive">
+                            <table class="table table-striped mb-0">
+                                <tr>
+                                    <th>Grade</th>
+                                    <th>Start Score</th>
+                                    <th>Remark</th>
+                                </tr>
+                                <tr>
+                                    <th>A</th>
+                                    <td><input type="number" class="form-control form-control-sm" style="width: 60px"></td>
+                                    <td><input type="text" class="form-control form-control-sm"></td>
+                                </tr>
+                                <tr>
+                                    <th>B</th>
+                                    <td><input type="number" class="form-control form-control-sm" style="width: 60px"></td>
+                                    <td><input type="text" class="form-control form-control-sm"></td>
+                                </tr>
+                                <tr>
+                                    <th>C</th>
+                                    <td><input type="number" class="form-control form-control-sm" style="width: 60px"></td>
+                                    <td><input type="text" class="form-control form-control-sm"></td>
+                                </tr>
+                                <tr>
+                                    <th>D</th>
+                                    <td><input type="number" class="form-control form-control-sm" style="width: 60px"></td>
+                                    <td><input type="text" class="form-control form-control-sm"></td>
+                                </tr>
+                                <tr>
+                                    <th>E</th>
+                                    <td><input type="number" class="form-control form-control-sm" style="width: 60px"></td>
+                                    <td><input type="text" class="form-control form-control-sm"></td>
+                                </tr>
+                                <tr>
+                                    <th>F</th>
+                                    <td><input type="number" class="form-control form-control-sm" style="width: 60px"></td>
+                                    <td><input type="text" class="form-control form-control-sm"></td>
+                                </tr>
+                                <tr>
+                                    <td colspan="3">
+                                        <button class="btn btn-secondary float-right ">Submit</button>
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+                        {{-- <form class="form-horizontal">
+                            <div class="card-body">
+                                <div class="form-group row">
+                                    <label class="col-sm-2 col-form-label">A</label>
+                                    <div class="col-sm-4">
+                                        <input type="number" class="form-control" placeholder="A">
+                                    </div>
+                                    <label for="inputPassword3" class="col-sm-2 col-form-label">B</label>
+                                    <div class="col-sm-4">
+                                        <input type="number" class="form-control" placeholder="B">
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- /.card-body -->
+                            <div class="card-footer">
+                                <button type="submit" class="btn btn-info">Sign in</button>
+                                <button type="submit" class="btn btn-default float-right">Cancel</button>
+                            </div>
+                            <!-- /.card-footer -->
+                        </form> --}}
+                    </div>
+                </div>
+
+            </div>
+
         </div>
     </section>
 
@@ -88,18 +167,21 @@
                 ca3 = parseInt($(form).find('input[name="ca3"]').val())
                 exam = parseInt($(form).find('input[name="exam"]').val())
                 console.log(ca1, ca2, ca3, exam);
-                total = ca1+ca2+ca3+exam;
-                if(total != 100) { littleAlert('The sum of the socres must be equal to 100',1); return;}
+                total = ca1 + ca2 + ca3 + exam;
+                if (total != 100) {
+                    littleAlert('The sum of the socres must be equal to 100', 1);
+                    return;
+                }
                 $.ajax({
                     method: 'post',
-                    url: api_url+'update_ca',
+                    url: api_url + 'update_ca',
                     data: {
                         ca1: ca1,
                         ca2: ca2,
                         ca3: ca3,
                         exam: exam
                     },
-                    beforeSend:() => {
+                    beforeSend: () => {
                         btnProcess('.updateCa', '', 'before')
                     }
                 }).done(function(res) {
@@ -116,7 +198,7 @@
             function fetchCa() {
                 $.ajax({
                     method: 'get',
-                    url: api_url+'ca'
+                    url: api_url + 'ca'
                 }).done(function(res) {
                     console.log(res);
                     ca = res.data
